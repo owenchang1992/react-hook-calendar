e0e5807b1fa2eb3d17d073ef18cb949de3d32b99
Owen Chang <owenchang.dev@gmail.com>
2021-10-21 18:07:19 +0800

    Feat: generate day list

--- src/costomHook/useCalendar.js
+++ src/costomHook/useCalendar.js
@@ -1,9 +1,9 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 
 const useCalendar = () => {
-  const [today, setToday] = useState(new Date())
   const [month, setMonth] = useState(new Date().getMonth())
   const [year, setYear] = useState(new Date().getFullYear())
+  const [days, setDays] = useState(null)
 
   const monthForward = () => {
     if (month === 11) {
@@ -27,10 +27,45 @@ const useCalendar = () => {
     setMonth(month - 1)
   }
 
+  const getDays = () => {
+    const daysInMonth = 32 - new Date(year, month, 32).getDate()
+    const firstDayIndex = new Date(year, month).getDay()
+    const firstDayOfList = new Date(year, month, -firstDayIndex).getDate() + 1
+    const lastDayIndex = daysInMonth + firstDayIndex - 1
+    const newDays = []
+
+    const addDays = (days, initDay, length) => {
+      for(let i=initDay; i < initDay + length; i++) {
+        days.push(i)
+      }
+
+      return days
+    }
+
+    // add days of previous month to newDays
+    addDays(newDays, firstDayOfList, firstDayIndex)
+
+    // add days of current month to newDays
+    addDays(newDays, 1, daysInMonth)
+
+    // add days of next month to newDays
+    addDays(newDays, 1, 41 - lastDayIndex)
+
+    setDays({
+      firstDayIndex,
+      lastDayIndex,
+      dayList: newDays
+    })
+  }
+
+  useEffect(() => {
+    getDays()
+  }, [year, month])
+
   return {
-    today,
     month,
     year,
+    days,
     monthForward,
     monthBackward,
   }
