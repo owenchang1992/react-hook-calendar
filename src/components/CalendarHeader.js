9f672bce92ad52f316398a8c4cc0f35f66d7268a
Owen Chang <owenchang.dev@gmail.com>
2021-10-21 16:17:47 +0800

    Feat: use svg as arrow

--- src/components/CalendarHeader.js
+++ src/components/CalendarHeader.js
@@ -1,14 +1,28 @@
 import React from 'react'
 import '../styles/Calendar.css'
-import ArrowBackIosIcon from '@mui/icons-material/ArrowBackIos'
-import ArrowForwardIosIcon from '@mui/icons-material/ArrowForwardIos'
+
+const Icon = ({path, onClick}) => (
+  <svg 
+    className="icon"
+    viewBox="0 0 24 24"
+    onClick={onClick}
+  >
+    {path}
+  </svg>
+)
 
 const CalenderHeader = ({ title, onBackClick, onForwardClick, onTitleClick }) => {
   return (
     <div className="header">
-      <ArrowBackIosIcon onClick={onBackClick}/>
-      <h5 onClick={onTitleClick}>{title}</h5>
-      <ArrowForwardIosIcon onClick={onForwardClick}/>
+      <Icon
+        path={ <path d="M15.41 7.41 14 6l-6 6 6 6 1.41-1.41L10.83 12z" /> }
+        onClick={onBackClick}
+      />
+      <div className="header-title" onClick={onTitleClick}>{title}</div>
+      <Icon 
+        path={ <path d="M10 6 8.59 7.41 13.17 12l-4.58 4.59L10 18l6-6z" /> }
+        onClick={onForwardClick}
+      />
     </div>
   )
 }
